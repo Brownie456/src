38fe9bb52f80fcd00b149d976f4ddf0c1ab9bc5c
Brownie456 <kbrown23@albany.edu>
2017-05-01 16:10:16 -0400

    commit 2
    finished search and delete methods

--- TreeClass.java
+++ TreeClass.java
@@ -2,4 +2,15 @@
  * Created by Brownie on 5/1/2017.
  */
 public class TreeClass {
+
+    public static void search(){
+        System.out.println("found");
+
+    }
+
+    public static void delete(){
+        System.out.println("deleted");
+
+
+    }
 }
